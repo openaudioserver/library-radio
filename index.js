0a4eddcf82d4e97f7d739d8f00b4922ae06ecf96
randomdev1234 <randomdev1234@protonmail.com>
2021-08-05 11:37:35 -0600

    WIP

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ module.exports = {
       get: require('./api/radiostations.get.js'),
       list: require('./api/radiostations.list.js'),
       remove: require('./api/radiostations.remove.js'),
-      reorder: require('./api/radiostations.reorder.js'),
       update: require('./api/radiostations.update.js')
     }
     return library
